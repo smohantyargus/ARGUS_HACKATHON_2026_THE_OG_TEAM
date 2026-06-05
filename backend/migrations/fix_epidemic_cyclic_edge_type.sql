81285cb1b064258630e987cc746760fb1d68d83a
Jay Prajapati <jprajapati@argusoft.com>
2026-06-05 22:07:06 +0530

    fix in epidemic_cyclic_egde sql

--- backend/migrations/fix_epidemic_cyclic_edge_type.sql
+++ backend/migrations/fix_epidemic_cyclic_edge_type.sql
@@ -3,10 +3,13 @@
 -- in the targets set, making it invisible as the pipeline entry node.
 UPDATE pipeline_edges pe
 SET edge_type = 'cyclic_feedback'
-FROM pipeline_nodes src
-JOIN pipeline_nodes tgt ON tgt.node_key = 'Epidemiologist'
-JOIN pipeline_definitions pd ON pd.name = 'epidemic_containment' AND pd.id = pe.pipeline_id
-WHERE pe.source_node_id = src.id
+FROM pipeline_nodes src,
+     pipeline_nodes tgt,
+     pipeline_definitions pd
+WHERE pd.name = 'epidemic_containment'
+  AND pe.pipeline_id = pd.id
+  AND pe.source_node_id = src.id
   AND src.node_key = 'policy_aggregator'
+  AND tgt.node_key = 'Epidemiologist'
   AND pe.target_node_id = tgt.id
   AND pe.edge_type = 'sequential';
